84705b24edd33ef9959359760062071e7115db55
rosemo21 <rosemolpp2001@gmail.com>
2025-01-03 11:57:12 +0530

    last commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
+const helmet=require('helmet');
 const port = process.env.PORT || 3000;
 const viewhomepage=require('./routes/home')
 const viewdatabasetable=require('./routes/viewdata')
 const viewabout = require('./routes/about');
 const viewuser = require('./routes/user');
+server.use(helmet());
 server.use('/',viewhomepage);
 server.use('/database',viewdatabasetable);
 server.use('/about',viewabout);
