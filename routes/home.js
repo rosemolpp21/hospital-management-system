52db844b109ec2f24bd2017017035120cf099ced
rosemo21 <rosemolpp2001@gmail.com>
2025-01-06 20:25:29 +0530

    CRUD OPERATION

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
 router.get('/',(req,res)=>{
-    res.send(`this is my home page <br>type /database to view database table <br> type /about to go to about page <br> type /users to go to user page`)
+    res.send(`this is my home page`)
 })
 module.exports=router;
