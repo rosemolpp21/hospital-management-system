ac55bbcc1c87b2890ea3dea79b8cf9388e647099
rosemo21 <rosemolpp2001@gmail.com>
2025-01-06 18:53:08 +0530

    CRUD OPERATION

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,81 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send('This is the users page');
+const db = require('../config/db-connect');
+router.post('/', (req, res) => {
+    const ID = req.body.ID;
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const phone_no = req.body.phone_no;
+    const gender = req.body.gender;
+    const age = req.body.age;
+    const address = req.body.address;
+    if (!ID || !first_name || !last_name || !phone_no || !gender || !age || !address) {
+        return res.status(500).send('all fields required');
+    }
+    db.query(`INSERT INTO patient (ID, first_name, last_name, phone_no, gender, age, address, created_at)
+    VALUES (?, ?, ?, ?, ?, ?, ?, NOW())`, [ID, first_name, last_name, phone_no, gender, age, address], (err, result) => {
+        if (err) {
+            return res.status(500).send('error in inserting patient');
+        }
+        res.status(200).send('patient added susccesfully');
+    });
 });
-router.get('/:id',(req,res)=>{
-   res.send(`this user with id ${req.params.id}`)
-})
+
+
+router.put('/:id', (req, res) => {
+    const ID = req.params.id;
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const phone_no = req.body.phone_no;
+    const gender = req.body.gender;
+    const age = req.body.age;
+    const address = req.body.address;
+    const updatefield = [];
+    const values = [];
+    if (first_name != undefined) {
+        updatefield.push('first_name=?');
+        values.push(first_name);
+    }
+    if (last_name != undefined) {
+        updatefield.push('last_name=?');
+        values.push(last_name);
+    }
+    if (phone_no != undefined) {
+        updatefield.push('phone_no=?');
+        values.push(phone_no);
+    }
+    if (gender != undefined) {
+        updatefield.push('gender=?');
+        values.push(gender);
+    }
+    if (age != undefined) {
+        updatefield.push('age=?');
+        values.push(age);
+    }
+    if (address != undefined) {
+        updatefield.push('address=?');
+        values.push(address);
+    }
+    if (updatefield.length === 0) {
+        return res.status(500).send("minimum 1 field required");
+        
+    }
+    values.push(ID);
+    db.query(`UPDATE patient SET ${updatefield.join(', ')} WHERE ID = ?`, values, (err, result) => {
+        if (err) {
+            return res.status(500).send('error in updating patient');
+        }
+        res.status(200).send(`updated data successfully`);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const ID = req.params.id;
+    db.query(`DELETE FROM patient WHERE ID = ?`, [ID], (err, result) => {
+        if (err) {
+            return res.status(500).send('error in deleting patient');
+        }
+        res.status(200).send(`patient with ID: ${ID} deleted from database`);
+    });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const server = express();
 const helmet=require('helmet');
 const port = process.env.PORT || 3000;
 const viewhomepage=require('./routes/home')
-const viewdatabasetable=require('./routes/viewdata')
-const viewabout = require('./routes/about');
 const viewuser = require('./routes/user');
+server.use(express.json());
 server.use(helmet());
 server.use('/',viewhomepage);
-server.use('/database',viewdatabasetable);
-server.use('/about',viewabout);
 server.use('/users', viewuser);
 server.all('*', (req, res) => {
     res.status(404).send('error! content is not available');
@@ -17,3 +14,4 @@ server.all('*', (req, res) => {
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+module.exports=server;
